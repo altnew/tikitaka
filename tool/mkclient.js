6821dfa26746ea878dc2253fa3363fa12f3bb630
Ken Mat <ken.ma24ma@gmail.com>
2014-03-24 08:32:29 +0900

    add the code making client js

--- tool/mkclient.js
+++ tool/mkclient.js
@@ -1,10 +1,9 @@
 var exec = require('child_process').exec,
 	fs = require('fs');
 
-var lst = { 'netport.js' : false, 'objstore.js' : false, 'client/proxy.js' : false };
+var lst = { 'netport.js' : false, 'objstore.js' : false };
 const yuiname = 'yuicompressor-2.4.8.jar';
 const spth = 'java -jar lib/' + yuiname + ' ../lib/';
-//const dpth = '../tmp/client.js';
 const base = '../lib/tikitaka.jspart';
 const sfile = '../lib/tikitaka.js';
 var buf = '';
@@ -48,4 +47,19 @@ for ( var i in lst ) {
 	});
 }
 
+exec(spth + 'client/proxy.js', function(err, stdout, stderr) {
+	if (!err) {
+		stdout = stdout.replace(/[a-zA-Z0-9]+\$([a-zA-Z][a-zA-Z0-9]*)/mg, '$1');
+		fs.writeFile('../tmp/proxy.min.js',stdout,
+		 function(err) {
+			 if (err) {
+				console.log('failure to save proxy.js');
+			 } else {
+				console.log('succeded to save proxy.js');
+			 }
+		});
+	} else {
+		console.log(i + ':' + err);
+	}
+})
 
